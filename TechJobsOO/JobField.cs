bc5d70e13ee7a3f601db1dd1eb829f3bef865b7c
dantehargrow <dantehargrow@gmail.com>
2020-11-02 23:33:46 -0600

    Created abstract class

---
+++ TechJobsOO/JobField.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechJobsOO
+{
+    public abstract class JobField
+    {
+        int Id { get; }
+        static int nextId = 1;
+        string Value { get; set; }
+
+        public JobField()
+        {
+            Id = nextId;
+            nextId++;
+        }
+        public JobField(string value) : this()
+        {
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is JobField jobField &&
+                   Id == jobField.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+}
